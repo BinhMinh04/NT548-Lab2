34d87e945c4114fcc2161222374827e5aa726e09
Trần Gia Bảo <142662904+Giabaoday@users.noreply.github.com>
2025-06-14 19:42:03 +0700

    Update main.tf

--- Task1/main.tf
+++ Task1/main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "remote-backend-s3"
+    key            = "terraform/terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "remote-backend-locks"
+    encrypt        = true
+  }
+}
+
 module "vpc" {
   source = "./modules/vpc"
 
